453048542c6fd29e4fb74eaaebf7f09c4aa1fcf9
Frozen Esper <frznesper@gmail.com>
2012-11-07 15:52:02 -0500

    Ignore testing Console changes

--- MusicArtDownloader.Console/Program.cs
+++ MusicArtDownloader.Console/Program.cs
@@ -11,9 +11,27 @@ namespace MusicArtDownloader.Console
 {
     class Program
     {
+        const string radioheadFileName = @"C:\Users\sachin\Documents\Visual Studio 2012\Projects\mad\MusicArtDownloader.Test\radiohead.xml";
+        const string discoveryFileName = @"C:\Users\sachin\Documents\Visual Studio 2012\Projects\mad\MusicArtDownloader.Test\discovery-lp.xml";
+
         static void Main(string[] args)
         {
+            var fanart = new FanartContext();
+            var music = fanart.Music;
+            var paths = new string[] { radioheadFileName, discoveryFileName };
 
+            var artists = paths.Select(p => File.ReadAllText(p))
+                               .Select(x => music.GetArtistFromXml(x))
+                               .ToList();
+
+            var xml = music.GetXmlFromArtists(artists);
+            var roundtrip = music.GetArtistsFromXml(xml).ToList();
+            var xml2 = music.GetXmlFromArtists(roundtrip);
+
+            var r1 = artists.Last();
+            var r2 = roundtrip.Last();
+
+            var e = artists.SequenceEqual(roundtrip);
         }
     }
 }
